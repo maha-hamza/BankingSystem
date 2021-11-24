7bd34923fba80eb275e212e42cfe46fb9dc4c4af
Maha Hamza <m.hamza@scopegroup.com>
2021-11-24 16:03:37 +0100

    First Commit

---
+++ src/main/kotlin/com/dkb/bankingsystem/BankingSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.dkb.bankingsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankingSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankingSystemApplication>(*args)
+}
